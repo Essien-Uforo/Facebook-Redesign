22b1fc3e0d57a74f5c81a2d0ca8c11aaec13f7f2
Essien-Uforo <essienuforoabasi@gmail.com>
2021-07-08 13:32:31 +0100

    initial commit

---
+++ facebook_login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Facebook</title>
+
+<link rel="stylesheet" href="style.css">
+
+
+
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="main">
+
+        <div class="main-left">
+
+            <p class="facebook-logo">facebook</p>
+
+            <p class="facebook-status">Facebook helps you connect and share with the people in your life.</p>
+        
+
+        </div>
+        <div class="main-right">
+            <div class="main-right-login">
+
+            <div class="main-right-email">
+                <input type="email" placeholder="Email address or phone number">
+            </div>
+
+            <div class="main-right-password">
+                <input type="password" placeholder="Password">
+            </div>
+            <div class="main-right-button">
+               <button>
+                   Log In
+               </button> 
+            </div>
+
+            <div class="main-right-link">
+                <a href="">Forgotten account?</a>
+            </div>
+
+            <div class="main-right-line">
+
+            </div>
+
+            <div class="main-right-account">
+                <button>Create New Account</button>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="footer">
+        <?php include('footer.php'); ?>
+    </div>
+    <div class="modal">
+        
+    </div>
+
+</body>
+</html>
